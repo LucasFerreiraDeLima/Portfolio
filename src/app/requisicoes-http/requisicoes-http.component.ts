53caa76db358c74b11e431fe7501e7e477c754f8
Lucas Ferreira de Lima <lucaslima0803@gmail.com>
2023-05-12 09:20:17 -0300

    second commit

--- src/app/requisicoes-http/requisicoes-http.component.ts
+++ src/app/requisicoes-http/requisicoes-http.component.ts
@@ -9,18 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class RequisicoesHttpComponent implements OnInit{
 
   public getJson: any;
-  public idUser: any;
+  public postJson: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getJsonInfo()
+    this.getJsonInfo();
+    this.postJsonInfo();
   }
 
   getJsonInfo() {
     this.http.get('https://reqres.in/api/users?page=2').subscribe((data) => {
       console.log(data);
       this.getJson = data;
+    })
+  }
+
+  postJsonInfo() {
+    let body = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }
+
+    this.http.post('https://jsonplaceholder.typicode.com/posts', body).subscribe((data) => {
+      console.log(data);
+      return this.postJson = data;
     })
   }
 }
